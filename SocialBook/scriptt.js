f66dfd865d3d7d04b222c70de9bc8bc41bb5078f
Surafel <surafelmoges01@gmail.com>
2025-03-11 11:03:43 +0000

    Finish Home page

--- SocialBook/scriptt.js
+++ SocialBook/scriptt.js
@@ -7,5 +7,24 @@ function toggleSetting(){
 
 darkBtn.onclick = function(){
     darkBtn.classList.toggle('dark-btn-on');
+    document.body.classList.toggle('dark-theme');
+
+    if(localStorage.getItem('theme') == 'light'){
+        localStorage.setItem('theme', 'dark');
+    }
+    else{
+        localStorage.setItem('theme', 'light');
+    }
 }
+
+
+if(localStorage.getItem('theme') == 'light'){
+    darkBtn.classList.remove('dark-btn-on');
+    document.body.classList.remove('dark-theme');
+}
+else if(localStorage.getItem('theme') == 'dark'){
+    darkBtn.classList.add('dark-btn-on');
+    document.body.classList.add('dark-theme');
+}
+
 
